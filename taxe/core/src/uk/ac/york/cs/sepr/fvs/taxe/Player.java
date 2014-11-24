875ec51ba78a8a60694d07355a69cf6c92cc95e9
Juddling <juddling@assassinators.net>
2014-11-24 15:38:34 +0000

    added some basic types

---
+++ taxe/core/src/uk/ac/york/cs/sepr/fvs/taxe/Player.java
@@ -0,0 +1,17 @@
+package uk.ac.york.cs.sepr.fvs.taxe;
+
+import java.util.List;
+
+public class Player {
+	private int score;
+	private List<Resource> resources;
+	private List<Goal> goals;
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public List<Resource> getResources() {
+		return resources;
+	}
+}
---
+++ taxe/core/src/uk/ac/york/cs/sepr/fvs/taxe/Resource.java
@@ -0,0 +1,5 @@
+package uk.ac.york.cs.sepr.fvs.taxe;
+
+public abstract class Resource {
+	private String name;
+}
