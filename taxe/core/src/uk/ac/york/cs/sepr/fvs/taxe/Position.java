2fcc8201412d02588f1a9642ca02ebe793fb69af
Owen Hurford <alwaysbusy@users.noreply.github.com>
2014-11-25 23:10:57 +0000

    Make Position coords not final

--- taxe/core/src/uk/ac/york/cs/sepr/fvs/taxe/Position.java
+++ taxe/core/src/uk/ac/york/cs/sepr/fvs/taxe/Position.java
@@ -1,8 +1,8 @@
 package uk.ac.york.cs.sepr.fvs.taxe;
 
 public class Position {
-	public final int x;
-	public final int y;
+	public int x;
+	public int y;
 	public Position(int x, int y){
 		this.x = x;
 		this.y = y;
