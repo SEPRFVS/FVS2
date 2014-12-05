ba2f1741a45e4aca5f03b0c315af9ba7c7ee6586
Owen Hurford <alwaysbusy@users.noreply.github.com>
2014-12-05 11:59:02 +0000

    Add visual representation of connection

--- taxe/core/src/uk/ac/york/cs/sepr/fvs/taxe/Map.java
+++ taxe/core/src/uk/ac/york/cs/sepr/fvs/taxe/Map.java
@@ -29,15 +29,17 @@ public class Map extends ScreenAdapter {
 			//Configure variables
 			this.location = new Position(x,y);
 			this.relation = relation;
-			//Set image
+			//Set image, Size and Name
 			String image;
-			
+			size = new Size(32,32);
 			
 			if(this.relation instanceof Station){
 				image = "station.png";
-				size = new Size(32,32);
+				this.setName(((Station) this.relation).getName());
 			}else if(this.relation instanceof Connection){
 				image = "connection.png";
+				this.setName(((Connection) this.relation).getStation1().getName() + "-" + ((Connection) this.relation).getStation2().getName());
+				this.location = new Position(x+16,y+16);
 			}else{
 				image = "missing.png";
 			}
@@ -53,6 +55,16 @@ public class Map extends ScreenAdapter {
 					return true;
 				}
 			});
+		}
+		
+		//Expand connection to correct size
+		public void expandConnection(IPositionable lowerCorner, IPositionable upperCorner){
+			//Set sprite size
+			Size size = new Size(upperCorner.getX() - lowerCorner.getX(), upperCorner.getY() - lowerCorner.getY());
+			this.size = size;
+			//Rotate rails using SOHCAHTOA
+			double rotation = Math.toDegrees(Math.atan(size.getWidth()/size.getHeight()));
+			this.setRotation((float) rotation); //TODO Doesn't actually rotate
 		}
 		
 		//Draw actor on each draw call
@@ -74,10 +86,12 @@ public class Map extends ScreenAdapter {
 	final private TaxeGame game;
 	private OrthographicCamera camera;
 	private Stage stage; // Stage - Holds all actors
+	private Group actors; //Group - Hold all actors to allow searching and redrawing
 	private Vector3 touchPoint;
 	private Texture mapTexture;
 	private Image mapImage;
 	private List<Station> stations;
+	private List<Connection> connections;
 
 	public Map(TaxeGame game){
 		this.game = game;
@@ -85,11 +99,12 @@ public class Map extends ScreenAdapter {
 		camera = new OrthographicCamera(TaxeGame.WIDTH, TaxeGame.HEIGHT);
 		camera.setToOrtho(false); // Makes the origin to be in the lower left corner
 		stage = new Stage();
+		actors = stage.getRoot();
 		touchPoint = new Vector3();
 		mapTexture = new Texture(Gdx.files.internal("gamemap.png"));
 		mapImage = new Image(mapTexture);
 		stations = new ArrayList<Station>();
-
+		connections = new ArrayList<Connection>();
 
 	}
 	
@@ -117,11 +132,66 @@ public class Map extends ScreenAdapter {
 		IPositionable location = station.getLocation();
 		MapActor actor = new MapActor(location.getX(), location.getY(), station);
 		actor.setTouchable(Touchable.enabled);
-		stage.addActor(actor);
+		actors.addActor(actor);
+	}
+	
+	//Get Connections
+	public List<Connection> getConnections(){
+		return connections;
+	}
+	
+	//Add Connection
+	public Connection addConnection(Station station1, Station station2){
+		Connection newConnection = new Connection(station1, station2);
+		connections.add(newConnection);
+		return newConnection;
+	}
+	
+	//Add Connection by Names
+	public Connection addConnection(String station1, String station2){
+		Station st1 = getStationByName(station1);
+		Station st2 = getStationByName(station2);
+		return addConnection(st1, st2);
+	}
+	
+	//Get connections from station
+	@SuppressWarnings("null")
+	public List<Connection> getConnectionsFromStation(Station station){
+		List<Connection> results = null;
+		for(Connection connection : connections){
+			if(connection.getStation1() == station || connection.getStation2() == station){
+				results.add(connection);
+			}
+		}
+		return results;
+	}
+	
+	//Display all connections
+	public void renderConnections(){
+		for(Connection connection : connections){
+			renderConnection(connection);
+		}
+	}
+	
+	//Display Individual connection
+	public void renderConnection(Connection connection){
+		IPositionable lowerCorner, upperCorner;
+		if(connection.getStation1().getLocation().getX() < connection.getStation2().getLocation().getY()){
+			//Lower position at X
+			lowerCorner = connection.getStation1().getLocation();
+			upperCorner = connection.getStation2().getLocation();
+		}else{
+			//Lower position at Y
+			lowerCorner = connection.getStation2().getLocation();
+			upperCorner = connection.getStation1().getLocation();
+		}
+		MapActor actor = new MapActor(lowerCorner.getX(), lowerCorner.getY(), connection);
+		actor.expandConnection(lowerCorner, upperCorner);
+		actors.addActor(actor);
 	}
 	
 	//Get Station by Name (May or may not be needed)
-	/*public Station getStationByName(String name){
+	public Station getStationByName(String name){
 		int i = 0;
 		boolean found = false;
 		while(i < stations.size() && found == false){
@@ -132,10 +202,8 @@ public class Map extends ScreenAdapter {
 				i++;
 			}
 		}
-		if(found = false){
-			return null;
-		}
-	}*/
+		return null;
+	}
 	
 	//Render Screen on load
 	@Override
@@ -156,9 +224,14 @@ public class Map extends ScreenAdapter {
 		mapImage.setHeight(TaxeGame.HEIGHT);
 		stage.addActor(mapImage);
 		
-		//Add Stations
-		addStation("Test Station", new Position(150, 50));
+		//Add Stations - TODO Make a useful implementation
+		addStation("Madrid", new Position(150, 50));
+		addStation("Paris", new Position(300,220));
 		renderStations();
+		
+		//Add Connections - TODO Make a useful implementation
+		addConnection("Paris","Madrid");
+		renderConnections();
 	}
 
 
