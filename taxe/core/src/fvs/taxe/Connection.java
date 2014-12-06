37f0e431d81945bb6b06cf52905c9911901b5ab2
James Judd <juddling@assassinators.net>
2014-12-06 23:16:14 +0000

    removed useless comments and changed typo

--- taxe/core/src/fvs/taxe/Connection.java
+++ taxe/core/src/fvs/taxe/Connection.java
@@ -9,13 +9,11 @@ public class Connection {
 	private Station station2;
 	private List<Train> users;
 	
-	//Initialise
 	public Connection(Station station1, Station station2){
 		this.station1 = station1;
 		this.station2 = station2;
 	}
 	
-	//Station1 Mutators
 	public void setStation1(Station station){
 		this.station1 = station;
 	}
@@ -24,8 +22,7 @@ public class Connection {
 		return this.station1;
 	}
 	
-	//Station2 Mutators
-	public void setSation2(Station station){
+	public void setStation2(Station station){
 		this.station2 = station;
 	}
 	
@@ -33,17 +30,14 @@ public class Connection {
 		return this.station2;
 	}
 	
-	//Users accessor
 	public List<Train> getUsers(){
 		return this.users;
 	}
 	
-	//Add to Users
 	public void addUser(Train train){
 		this.users.add(train);
 	}
 	
-	//Remove from Users
 	public void removeUser(Train train){
 		this.users.remove(train);
 	}
