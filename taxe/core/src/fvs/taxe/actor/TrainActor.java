e3fe990985c12bebfc91899ab65fd23f53687cc9
manfredve <ml1118@york.ac.uk>
2015-01-11 16:15:02 +0000

    Trains now turn based on their moving direction

--- taxe/core/src/fvs/taxe/actor/TrainActor.java
+++ taxe/core/src/fvs/taxe/actor/TrainActor.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import gameLogic.Game;
 import gameLogic.GameState;
 import gameLogic.map.IPositionable;
@@ -13,10 +14,16 @@ public class TrainActor extends Image {
     public static int width = 36;
     public static int height = 36;
 
-    Rectangle bounds;
+    private Rectangle bounds;
+    public boolean facingLeft;
+    private float previousX;
+    private Drawable leftDrawable;
+    private Drawable rightDrawable;
 
     public TrainActor(Train train) {
-        super(new Texture(Gdx.files.internal(train.getImage())));
+        super(new Texture(Gdx.files.internal(train.getLeftImage())));
+        leftDrawable = getDrawable();
+        rightDrawable = new Image(new Texture(Gdx.files.internal(train.getRightImage()))).getDrawable();
 
         IPositionable position = train.getPosition();
 
@@ -24,6 +31,8 @@ public class TrainActor extends Image {
         setSize(width, height);
         bounds = new Rectangle();
         setPosition(position.getX() - width / 2, position.getY() - height / 2);
+        previousX = getX();
+        facingLeft = true;
     }
 
     @Override
@@ -31,11 +40,26 @@ public class TrainActor extends Image {
         if (Game.getInstance().getState() == GameState.ANIMATING) {
             super.act(delta);
             updateBounds();
+            updateFacingDirection();
         }
     }
 
     private void updateBounds() {
         bounds.set(getX(), getY(), getWidth(), getHeight());
+    }
+
+    public void updateFacingDirection() {
+        float currentX = getX();
+
+        if(facingLeft && previousX < currentX) {
+            setDrawable(rightDrawable);
+            facingLeft = false;
+        } else if(!facingLeft && previousX > currentX) {
+            setDrawable(leftDrawable);
+            facingLeft = true;
+        }
+
+        previousX = getX();
     }
 
     public Rectangle getBounds() {
