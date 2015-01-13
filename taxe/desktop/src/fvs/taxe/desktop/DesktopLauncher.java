fe17ccb4c0db997df0c8b6ea27877a0aabeed560
Owen Hurford <owen@hurfo.com>
2015-01-13 00:08:06 +0000

    Give window icon

--- taxe/desktop/src/fvs/taxe/desktop/DesktopLauncher.java
+++ taxe/desktop/src/fvs/taxe/desktop/DesktopLauncher.java
@@ -1,7 +1,9 @@
 package fvs.taxe.desktop;
 
+import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import fvs.taxe.TaxeGame;
 
 public class DesktopLauncher {
@@ -10,6 +12,12 @@ public class DesktopLauncher {
 		//Set window size
 		config.height = TaxeGame.HEIGHT;
 		config.width = TaxeGame.WIDTH;
+		config.title = "TaxE";
+		config.addIcon("icon/fvs256.png", FileType.Internal);
+		config.addIcon("icon/fvs128.png", FileType.Internal);
+		config.addIcon("icon/fvs64.png", FileType.Internal);
+		config.addIcon("icon/fvs32.png", FileType.Internal);
+		config.addIcon("icon/fvs16.png", FileType.Internal);
 		//config.fullscreen = true;
 		new LwjglApplication(new TaxeGame(), config);
 	}
